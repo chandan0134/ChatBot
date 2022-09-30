41a74d2138ac0b8832c974e69d5db5557fe799e1
chandan0134 <chandanma39@gmail.com>
2022-09-30 15:35:28 +0530

    commit

--- training.py
+++ training.py
@@ -1,1 +1,40 @@
 import nltk
+nltk.download('punkt')
+nltk.download('wordnet')
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import json
+import pickle
+
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.optimizers import SGD
+import random
+
+words=[]
+classes = []
+documents = []
+ignore_words = ['?', '!']
+
+data_file = open('data.json').read()
+intents = json.loads(data_file)
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        
+        w = nltk.word_tokenize(pattern)
+        documents.append((w, intent['tag']))
+        if intent['tag'] not in classes:
+              classes.append(intent['tag'])
+
+
+words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
+words = sorted(list(set(words)))
+
+classes = sorted(list(set(classes)))
+
+print (len(documents), "documents",documents)
+print (len(classes), "classes", classes)
+print (len(words), "unique lemmatized words", words)
+     
